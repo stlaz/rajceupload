a2dd485ca3467da14e71bebb534adab1a64b10a6
Stanislav Láznička <slaz@seznam.cz>
2014-12-03 18:00:43 +0100

    Nice new layout for uploading a new album.
    Signed-off-by: Stanislav Láznička <slaz@seznam.cz>

--- app/src/main/java/rajce/rajceUploader/NewAlbum.java
+++ app/src/main/java/rajce/rajceUploader/NewAlbum.java
@@ -1,48 +1,28 @@
 package rajce.rajceUploader;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 
 
-public class OldNewDialog extends Activity implements OnItemClickListener {
-    ListView listView;
+public class NewAlbum extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_old_new_dialog);
-
-        listView = (ListView) findViewById(R.id.oldnew_list);
-        listView.setOnItemClickListener(this);
+        setContentView(R.layout.activity_new_album);
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setIcon(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> adapter, View v, int position, long id) {
-        switch (position) {
-            case 0:
-                Intent i = new Intent(getApplicationContext(), NewAlbum.class);
-                startActivity(i);
-                break;
-            case 1:
-        }
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_old_new_dialog, menu);
+        getMenuInflater().inflate(R.menu.menu_new_album, menu);
         return true;
     }
 
